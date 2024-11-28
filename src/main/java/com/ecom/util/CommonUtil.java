4325f48aad468abf22bd9e5aee76578f0daf4626
Asif Al Fattah <asifalfattah34@gmail.com>
2024-11-28 16:43:55 +0600

    existing project

---
+++ src/main/java/com/ecom/util/CommonUtil.java
@@ -0,0 +1,41 @@
+package com.ecom.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import com.ecom.model.ProductOrder;
+import com.ecom.model.UserDtls;
+import com.ecom.service.UserService;
+
+// import jakarta.mail.MessagingException;
+// import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class CommonUtil {
+
+
+	@Autowired
+	private UserService userService;
+
+
+
+	public static String generateUrl(HttpServletRequest request) {
+		String siteUrl = request.getRequestURL().toString();
+		return siteUrl.replace(request.getServletPath(), "");
+	}
+
+	String msg = null;
+
+
+	public UserDtls getLoggedInUserDetails(Principal p) {
+		String email = p.getName();
+		UserDtls userDtls = userService.getUserByEmail(email);
+		return userDtls;
+	}
+}
